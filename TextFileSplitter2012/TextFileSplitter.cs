c38a8337bcff972c1293094efd6155cb6876f158
Keith Martin <keith.martin@outlook.com.au>
2017-07-09 22:39:42 +1000

    Fix 2016 missing UI, and add WIKI images.

--- TextFileSplitter2012/TextFileSplitter.cs
+++ TextFileSplitter2012/TextFileSplitter.cs
@@ -20,6 +20,9 @@ namespace Martin.SQLServer.Dts
         CurrentVersion = 5 // NB. Keep this in sync with ProvideCustomProperties and PerformUpgrade.
         , Description = "Extract many outputs from a single Text File"
         , IconResource = "Martin.SQLServer.Dts.Resources.TextFileSplitter.ico"
+#if SQL2016
+    , UITypeName = "Martin.SQLServer.Dts.TextFileSplitterForm, TextFileSplitter2016, Version=1.0.0.0, Culture=neutral, PublicKeyToken=cc8ffdd352b00674"
+#endif
 #if SQL2014
     , UITypeName = "Martin.SQLServer.Dts.TextFileSplitterForm, TextFileSplitter2014, Version=1.0.0.0, Culture=neutral, PublicKeyToken=cc8ffdd352b00674"
 #endif
